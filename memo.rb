c3d5d90d0a5b5534aca3b0e97cc8d70219f64c01
Jotaro <jotaro@JotaronoMacBook-Air.local>
2022-12-04 23:55:39 +0900

    Ruby実践課題(メモアプリ)の提出

---
+++ memo.rb
@@ -0,0 +1,34 @@
+require "csv"
+
+puts "1 → 新規でメモを作成 / 2 → 既存のメモを編集"
+memo_type = gets.to_i
+
+if memo_type == 1
+  puts "拡張子を除いた新規ファイル名を入力"
+  file_name = gets.chomp
+  if File.exist?("#{file_name}.csv")
+    puts "ファイルが既に存在しています"
+    return
+  else
+    CSV.open("#{file_name}.csv", "w") do |csv|
+      puts "メモしたい内容を入力（Ctrl + D で保存）"
+      memo = STDIN.read.chomp
+      csv << [memo.to_s]
+    end
+  end
+elsif memo_type == 2
+  puts "拡張子を除いた既存のファイル名を入力"
+  exist_file_name = gets.chomp
+  if File.exist?("#{exist_file_name}.csv") == false
+    puts "ファイルが存在しません"
+    return
+  else  
+    CSV.open("#{exist_file_name}.csv", "a") do |csv|
+      puts "メモしたい内容を入力（Ctrl + D で保存）"
+      memo = STDIN.read.chomp
+      csv << [memo.to_s]
+    end
+  end
+else
+  puts "1, 2どちらかを選択してください"
+end
